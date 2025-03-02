78a535e6d0ca030f181c0410598445cd1d0a93bf
Felix <59778334+FelixFester@users.noreply.github.com>
2025-03-02 19:33:20 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ ini_set('display_startup_errors', 0);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Felix's Corner</title>
+  <title>Your website name</title>
 </head>
 <body>
   <?php include 'cms/hentaicms.php'; ?>
