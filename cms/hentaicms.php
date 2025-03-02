56b25f9587aeaeeef5ecc6c84c246dc229f76184
Felix <59778334+FelixFester@users.noreply.github.com>
2025-03-02 19:33:39 +0300

    Add files via upload

--- cms/hentaicms.php
+++ cms/hentaicms.php
@@ -1,5 +1,5 @@
 <?php
-// Enable error reporting for debugging (disable in production)
+// Enable error reporting
 error_reporting(0);
 ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
