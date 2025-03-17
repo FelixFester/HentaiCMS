7ee917ab442eb8e759fca151dd053411fd9e86d0
Felix <59778334+FelixFester@users.noreply.github.com>
2025-03-17 03:52:52 +0300

    Add files via upload

--- settings.php
+++ settings.php
@@ -1,13 +1,13 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 $themes = array_filter(scandir('themes'), function ($file) {
     return pathinfo($file, PATHINFO_EXTENSION) === 'css';
 });
 
-$defaultTheme = 'default.css';
+$defaultTheme = 'Toxic.css';
 $currentTheme = isset($_COOKIE['hentaicms_theme']) ? htmlspecialchars($_COOKIE['hentaicms_theme']) : $defaultTheme;
 ?>
 <!DOCTYPE html>
@@ -16,7 +16,7 @@ $currentTheme = isset($_COOKIE['hentaicms_theme']) ? htmlspecialchars($_COOKIE['
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Settings</title>
-    <link rel="stylesheet" href="sakura.css">
+    <link rel="stylesheet" href="ヘンタイ CSS フレームワーク.css">
     <link id="themeCSS" rel="stylesheet" href="themes/<?php echo $currentTheme; ?>">
     <script>
         function changeTheme(theme) {
@@ -38,20 +38,20 @@ $currentTheme = isset($_COOKIE['hentaicms_theme']) ? htmlspecialchars($_COOKIE['
     <div class="echo-content">
         <div style="text-align: center;">
             <h1>Website Settings</h1>
-            <label for="theme">Select Theme:</label>
-            <select id="theme" onchange="changeTheme(this.value)">
-                <?php
-                foreach ($themes as $theme) {
-                    $selected = ($theme === $currentTheme) ? 'selected' : '';
-                    echo "<option value=\"$theme\" $selected>$theme</option>";
-                }
-                ?>
+        <label for="theme">Select Theme:</label>
+        <select id="theme" class="form-control" onchange="changeTheme(this.value)" style="max-width: 100%; overflow-x: auto;">
+        <?php
+    foreach ($themes as $theme) {
+        $selected = ($theme === $currentTheme) ? 'selected' : '';
+        echo "<option value=\"$theme\" $selected>$theme</option>";
+    }
+    ?>
             </select>
-            <br><br>
-            <button onclick="restoreDefault()">Restore Default</button>
-            <button onclick="window.location.href='index.php'">Go Back to Main Page</button>
+            <br>
+            <button class="button" onclick="restoreDefault()">Restore Default</button>  <button class="button" onclick="window.location.href='index.php'">Go Back to Main Page</button>
             <br>
         </div>
     </div>
 </body>
 </html>
+
